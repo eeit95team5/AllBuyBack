0a72b755e6e59faf08983f9bb44ccce0445dd66e
葉家銘 <x12345x_tw@hotmail.com>
2017-08-18 14:23:47 +0800

    商品頁面 填入中

--- AllBuyBack/src/main/java/com/allbuyback/shopshipway/model/ShopShipwayDAO.java
+++ AllBuyBack/src/main/java/com/allbuyback/shopshipway/model/ShopShipwayDAO.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -28,6 +31,45 @@ public class ShopShipwayDAO implements ShopShipwayDAOI {
 	
 	private static final String GET_ONE_SHOP =
 			"SELECT * FROM [SHOP_SHIPWAY] where s_id = ?";
+	private static final String GET_ONE_SHOP_JOIN = 
+			"SELECT * FROM SHOP_SHIPWAY JOIN SHIPWAY ON SHOP_SHIPWAY.sw_id=SHIPWAY.sw_id where s_id = ?";	
+	
+	
+	public Map selectJoin(int s_id) {
+		List list = new LinkedList();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Map m2 = new HashMap();
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(GET_ONE_SHOP_JOIN);
+			pstmt.setInt(1,s_id);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Map m1 = new HashMap();
+				m1.put("s_id", rs.getInt("s_id"));
+				m1.put("sw_id", rs.getInt("sw_id"));
+				m1.put("sw_name", rs.getString("sw_name"));
+				list.add(m1);
+			}
+			
+			m2.put("shipway", list);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally{
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		
+		return m2;
+	}
+	
 	@Override
 	public List<ShopShipwayVO> select(int s_id) {
 		List<ShopShipwayVO> list = new ArrayList<ShopShipwayVO>();
--- AllBuyBack/src/main/java/com/allbuyback/shopshipway/model/ShopShipwayDAOI.java
+++ AllBuyBack/src/main/java/com/allbuyback/shopshipway/model/ShopShipwayDAOI.java
@@ -1,7 +1,9 @@
 package com.allbuyback.shopshipway.model;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ShopShipwayDAOI {
 	public abstract List<ShopShipwayVO> select(int s_id);
+	public abstract Map selectJoin(int s_id);
 }
