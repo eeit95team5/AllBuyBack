995ff964c87f781dba7574c661ca9d9f3d93477a
ithill <ithill@MyWorkStation>
2017-08-22 13:01:27 +0800

    update MemberDAO

--- AllBuyBack/src/main/java/com/allbuyback/login/model/MemberDAO.java
+++ AllBuyBack/src/main/java/com/allbuyback/login/model/MemberDAO.java
@@ -332,9 +332,9 @@ public class MemberDAO {
 //			conn = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
 			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(UPDATELOGOUT);
-			java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
+			java.sql.Timestamp sqlDate = new java.sql.Timestamp(new java.util.Date().getTime());
 			System.out.println();
-			pstmt.setDate(1, sqlDate);
+			pstmt.setTimestamp(1, sqlDate);
 			pstmt.setInt(2, bean.getM_id());
 			result = pstmt.executeUpdate();			
 		} catch (SQLException e) {
